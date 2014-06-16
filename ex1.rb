d678867f2eb5090aad377cdc547757a02ec4a72b
tina <smilestar8@hotmail.com>
2014-06-16 15:51:55 +0800

    ruby exercise1-10

---
+++ ex1.rb
@@ -0,0 +1,13 @@
+a = 12  #number
+hash = ["happy", "sad", "shy" ]  #hash
+b = :language  #symbol
+
+puts "a"
+puts "#{a}"
+puts "#{b} :language"
+puts "#{hash}"
+puts "keep working"
+
+=begin
+didn't do 'mash','pound'
+=end
